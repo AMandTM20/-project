52a7ba159729bf71a007129e814fc51ad059f607
AMandTM20 <131997423+AMandTM20@users.noreply.github.com>
2023-08-09 19:42:37 +0300

    Add files via upload

---
+++ models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+
+class Phone(models.Model):
+    name = models.CharField(max_length=100)
+    price = models.PositiveIntegerField()
+    #image = models.URLField(max_length=100)# ссылка на фото модели
+    image = models.CharField(max_length=100)  # описание фото модели?
+    release_date = models.DateField( auto_now=False, auto_now_add=False)# время выпуска модели
+    lte_exists = models.BooleanField()
+    slug = models.SlugField(max_length=150, unique=True)
