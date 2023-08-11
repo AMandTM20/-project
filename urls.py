6e9b38749458a4f5b9a3057415bc235e47101d23
AMandTM20 <131997423+AMandTM20@users.noreply.github.com>
2023-08-11 14:42:49 +0300

    Add files via upload

--- urls.py
+++ urls.py
@@ -1,13 +1,10 @@
 from django.contrib import admin
 from django.urls import path
-from calculator import views
+from calculator.views import dish_view
 
 urlpatterns = [
      path('admin/', admin.site.urls),
-     path('index/', views.index, name='index'),
-     path('pasta/', views.pasta, name='pasta'),
-     path('omlet/', views.omlet, name='omlet'),
-     path('buter/', views.buter, name='buter'),
+     path('<dish>/', dish_view, name='dish')
 ]
 
 
